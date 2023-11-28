eda343d36e86e61a638fbcaec330ce3716927196
soumilnimbalkar26 <soumilnimblkar@gmail.com>
2023-11-28 18:08:02 +0530

    feat: :tada: inventory-app
    this is the beginning of the client side of the inventory-app

---
+++ inventory-app/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Dashboard from "./Dashboard/Dashboard"
+import { useState } from "react";
+
+function App() {
+  const [IsLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(IsLoggedIn);
+
+  const [IsSignedUp, setIsSignedUp] = useState(false);
+  console.log(IsSignedUp);
+
+  return (
+    <>
+      <Navbar
+        LoginVerified={IsLoggedIn}
+        // SignupVerified={IsSignedUp}
+        LoginCheck={setIsLoggedIn}
+        // SignUpCheck={setIsSignedUp}
+      />
+      <Routes>
+        <Route path="/login" element={<Login LoginCheck={setIsLoggedIn} />} />
+        <Route
+          path="/signup"
+          element={<SignUp SignupCheck={setIsSignedUp} />}
+        />
+        <Route path="/" element={<HomePage LoginVerified={IsLoggedIn} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
