3f9bc70b0b224ba1a498762267bf0a829b113d07
soumilnimbalkar26 <soumilnimblkar@gmail.com>
2023-12-06 13:31:27 +0530

    feat: :sparkles: sidebar
    rendered a sidebar on the dashboard page

--- inventory-app/src/Dashboard/Dashboard.jsx
+++ inventory-app/src/Dashboard/Dashboard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import DashboardSidebar from "../components/DashbboardSidebar";
+import AddUser from "../dashboardpages/addUser";
 
 const Dashboard = () => {
   return (
     <>
-      <DashboardSidebar />
+      <div className="flex">
+        <DashboardSidebar />
+        <Routes>
+          <Route path="/addUser" element={<AddUser />} />
+        </Routes>
+        <AddUser />
+      </div>
     </>
   );
 };
